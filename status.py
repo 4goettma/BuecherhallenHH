f823c1d767a4804386a107eabc50bb6cef5cf086
4goettma <4goettma@informatik.uni-hamburg.de>
2019-02-13 17:57:45 +0000

    Medientyp gefixt

--- status.py
+++ status.py
@@ -90,8 +90,8 @@ class konto():
             print("   Autor:")
 
         # Medienart
-        r4 = re.search("<p class=\"loans-media-type-text\">(?P<type>.*?)<\/p>", text)
-        if (len(re.findall("<p class=\"loans-media-type-text\">.*?<\/p>", text))):
+        r4 = re.search("<span class=\"loans-media-type-text\">(?P<type>.*?)<\/span>", text)
+        if (len(re.findall("<span class=\"loans-media-type-text\">.*?<\/span>", text))):
             if (r4.group("type") in ["Bestseller", "Blu-Ray-Disk", "DVD"]):
                 print("   Typ:",Style.BRIGHT+Fore.BLUE+r[0]+Style.RESET_ALL)
             else:
